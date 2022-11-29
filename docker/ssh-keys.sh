40ed3ec7ad7813bc11354b333e6d69e3cc15b3ce
Yim Lee <yim_lee@apple.com>
2022-11-29 12:39:32 -0800

    test

--- docker/ssh-keys.sh
+++ docker/ssh-keys.sh
@@ -3,7 +3,8 @@
 set -eu
 
 mkdir -p ~/.ssh
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-keyscan github.com >> /root/.ssh/known_hosts
+echo "Host *\n  StrictHostKeyChecking no" > /root/.ssh/config
 
 #cp -r /root/.ssh/* ~/.ssh/
 #chown -R $(id -u):$(id -g) ~/.ssh
