8333b7d503f3d8309beaddb5f84500b51342f565
David Nadoba <d_nadoba@apple.com>
2023-10-23 15:09:07 +0100

    Add benchmarks

---
+++ Benchmarks/Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version: 5.7
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftASN1 open source project
+//
+// Copyright (c) 2023 Apple Inc. and the SwiftASN1 project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftASN1 project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import PackageDescription
+
+let package = Package(
+    name: "benchmarks",
+    platforms: [
+        .macOS(.v13),
+    ],
+    dependencies: [
+        .package(path: "../"),
+        .package(url: "https://github.com/ordo-one/package-benchmark", from: "1.11.2"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "SwiftASN1Benchmark",
+            dependencies: [
+                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "SwiftASN1", package: "swift-asn1"),
+            ],
+            path: "Benchmarks/SwiftASN1Benchmark",
+            resources: [
+                .copy("ca-certificates/"),
+            ],
+            plugins: [
+                .plugin(name: "BenchmarkPlugin", package: "package-benchmark")
+            ]
+        ),
+    ]
+)
